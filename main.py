9c5c2fe26eddc7a032d29095171d5e27392e6e38
Ghost_Isaac072 <rtkjfhdc@hgtrbrfdtgfhj.ru>
2024-02-29 02:58:53 +0300

    final commit

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import os
-import random
-import sys
+from os import path, listdir
+from random import choice
+from math import radians, cos, sin, pi
+from sys import exit
 import pygame
+import pygame_gui
+
 pygame.init()
 
-TILE_SIZE = 32
+TILE_SIZE = 64
 SCREEN_INFO = pygame.display.Info()
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = SCREEN_INFO.current_w, SCREEN_INFO.current_h
 MAPS_DIR = "maps"
+MAPS_NUM = 1
 FPS = 60
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
+    fullname = path.join('data', name)
+    if not path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        exit()
     image = pygame.image.load(fullname)
 
     if colorkey is not None:
@@ -30,29 +34,35 @@ def load_image(name, colorkey=None):
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["СПАСИБО, ЧТО ЗАПУСТИЛИ ЭТОТ ШЕДЕВР",
+                  "Правила игры:",
+                  "Ваша задача - сломать все плохие шарики",
+                  "Управление платформой - мышка",
+                  "Запуск вашего шарика - SPACE или ЛКМ",
+                  "Рестарт вашего шарика - клавиша R",
+                  "Выход из игры - ESCAPE",
+                  "Для продолжения нажмите любую кнопку",
+                  "(Читы: клавиша D - удаление всех шаров)"]
 
-    background = pygame.transform.scale(load_image('start_screen.jpg'), (800, 600))
+    background = pygame.transform.scale(load_image('start_screen.jpg'), WINDOW_SIZE)
     screen.blit(background, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 200
+    font = pygame.font.Font(None, 36)
+    text_coord = 550
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
-        text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
+        intro_rect.x = 600
+        text_coord += intro_rect.height + 7
         screen.blit(string_rendered, intro_rect)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            if event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 return
         pygame.display.flip()
@@ -61,7 +71,149 @@ def start_screen():
 
 def terminate():
     pygame.quit()
-    sys.exit()
+    exit()
+
+
+def pause():
+    field = pygame.Surface([600, 300])
+    field.fill('grey')
+    screen.blit(field, (WINDOW_WIDTH // 2 - 300, WINDOW_HEIGHT // 2 - 150))
+    pygame.mouse.set_visible(True)
+    text = ["PAUSE", "Press SPACE to resume"]
+    font = pygame.font.Font(None, 60)
+    text_coord = WINDOW_HEIGHT // 2 - 50
+    for line in text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = WINDOW_WIDTH // 2 - 250
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                pygame.mouse.set_visible(False)
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def show_win_message():
+    global moving, MAPS_NUM, board
+    pygame.mouse.set_visible(True)
+
+    background = pygame.transform.scale(load_image('win_screen.jpg'), WINDOW_SIZE)
+
+    font = pygame.font.Font(None, 120)
+    text = font.render("YOU WON", 1, "darkblue")
+
+    manager = pygame_gui.UIManager(WINDOW_SIZE)
+
+    exit = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 550), (300, 100)),
+                                          text='Exit',
+                                          manager=manager)
+
+    restart = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 700), (300, 100)),
+                                           text='Restart',
+                                           manager=manager)
+
+    next_level = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((400, 700), (300, 100)),
+                                           text='Next level',
+                                           manager=manager)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == exit:
+                        terminate()
+                    if event.ui_element == restart:
+                        moving = False
+                        pygame.mouse.set_visible(False)
+                        player.angle = radians(45 + 90 * choice([0, 1]))
+                        board.render()
+                        pygame.mixer.music.load("sounds/bk_music.mp3")
+                        pygame.mixer.music.play(-1)
+                        return
+                    if event.ui_element == next_level:
+                        MAPS_NUM = (MAPS_NUM + 1) % len(listdir('maps/'))
+                        moving = False
+                        player.angle = radians(45 + 90 * choice([0, 1]))
+                        pygame.mouse.set_visible(False)
+                        board = Board(f"map_{MAPS_NUM}.txt")
+                        board.render()
+                        pygame.mixer.music.load("sounds/bk_music.mp3")
+                        pygame.mixer.music.play(-1)
+                        return
+            manager.process_events(event)
+        manager.update(clock.tick(FPS) / 1000)
+        screen.blit(background, (0, 0))
+        screen.blit(text, (1000, 100))
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+def show_lose_message():
+    global moving, board
+    pygame.mouse.set_visible(True)
+
+    background = pygame.transform.scale(load_image('lose_screen.jpg'), WINDOW_SIZE)
+
+    font = pygame.font.Font(None, 120)
+    text = font.render("YOU LOSE", 1, "darkblue")
+
+    manager = pygame_gui.UIManager(WINDOW_SIZE)
+
+    exit = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 400), (300, 100)),
+                                          text='Exit',
+                                          manager=manager)
+
+    restart = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 550), (300, 100)),
+                                           text='Restart',
+                                           manager=manager)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                terminate()
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == exit:
+                        terminate()
+                    if event.ui_element == restart:
+                        moving = False
+                        pygame.mouse.set_visible(False)
+                        player.angle = radians(45 + 90 * choice([0, 1]))
+                        board = Board(f"map_{MAPS_NUM}.txt")
+                        board.render()
+                        pygame.mixer.music.load("sounds/bk_music.mp3")
+                        pygame.mixer.music.play(-1)
+                        return
+            manager.process_events(event)
+        manager.update(clock.tick(FPS) / 1000)
+        screen.blit(background, (0, 0))
+        screen.blit(text, (50, 150))
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+def check_win():
+    if not all_balls.sprites():
+        return True
+
+
+def check_lose():
+    if player.rect.bottom > platform.rect.bottom:
+        return True
 
 
 class Ball(pygame.sprite.Sprite):
@@ -69,16 +221,16 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
         self.image = Ball.image
+        self.radius = TILE_SIZE // 2
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        #self.rect.
 
 
 class Board:
     def __init__(self, filename):
         self.map = []
-        with open(os.path.join(MAPS_DIR, filename)) as input_file:
+        with open(path.join(MAPS_DIR, filename)) as input_file:
             for line in input_file:
                 self.map.append((list(map(int, line.split()))))
         self.tile_size = TILE_SIZE
@@ -89,56 +241,70 @@ class Board:
             for x in range(len(self.map[y])):
                 if self.get_tile_id((x, y)):
                     ball = Ball(x * TILE_SIZE, y * TILE_SIZE)
-                    all_sprites.add(ball)
                     all_balls.add(ball)
+                    all_sprites.add(ball)
 
-
-                #rect = pygame.Rect(x * self.tile_size, y * self.tile_size,
-               #                    self.tile_size, self.tile_size)
-               # screen.fill(colors[self.get_tile_id((x, y))], rect)
     def get_tile_id(self, position):
         return self.map[position[1]][position[0]]
+
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(all_sprites)
-        self.x = WINDOW_WIDTH // 2 - 50
-        self.y = WINDOW_HEIGHT - 100
         self.image = pygame.Surface([200, 20], pygame.SRCALPHA, 32)
         self.image.fill('green')
         self.rect = self.image.get_rect()
-        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 20)
+        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 30)
 
     def update(self):
         self.rect.center = (pygame.mouse.get_pos()[0], WINDOW_HEIGHT - 50)
 
+
 class Player(pygame.sprite.Sprite):
-    image = pygame.transform.scale(load_image('player.png'), (50, 50))
+    image = pygame.transform.scale(load_image('player.png'), (TILE_SIZE // 2, TILE_SIZE // 2))
     def __init__(self):
         super().__init__(all_sprites)
-        self.x = WINDOW_WIDTH // 2 - 50
-        self.y = WINDOW_HEIGHT - 100
+        self.angle = radians(45 + 90 * choice([0, 1]))
+        self.radius = TILE_SIZE // 4
+        self.v = 10
         self.image = Player.image
         self.rect = self.image.get_rect()
-        self.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 30 - self.rect.height)
-        self.vx = 5 * random.choice([1, -1])
-        self.vy = -5
+        self.rect.center = (platform.rect.center[0], platform.rect.center[1] - self.rect.height // 2 + 1)
 
     def update(self):
         if not moving:
-            self.rect.center = (pygame.mouse.get_pos()[0], WINDOW_HEIGHT - 30 - self.rect.height)
+            self.rect.center = (platform.rect.center[0], platform.rect.center[1] - self.rect.height // 2 - 10)
         else:
-            self.rect = self.rect.move(self.vx, self.vy)
+            x, y = self.rect.center
+            self.rect.center = (x + self.v * cos(self.angle), y - self.v * sin(self.angle))
             if pygame.sprite.spritecollideany(self, horizontal_borders):
-                self.vy = -self.vy
+                self.angle = -self.angle
             if pygame.sprite.spritecollideany(self, vertical_borders):
-                self.vx = -self.vx
-            if pygame.sprite.spritecollideany(self, platform):
-                self.vy = -self.vy
-            hits = pygame.sprite.spritecollide(self, all_balls, True)
+                self.angle = pi - self.angle
+            if pygame.sprite.collide_rect(self, platform):
+                coef = (x - platform.rect.center[0]) / (platform.rect.width / 2)
+                if coef > 1:
+                    coef = (self.rect.x - platform.rect.center[0]) / (platform.rect.width / 2)
+                elif coef < -1:
+                    coef = (self.rect.right - platform.rect.center[0]) / (platform.rect.width / 2)
+                if 0.0 <= coef < 0.3:
+                    coef = 0.3
+                elif coef < -0.7:
+                    coef = -0.7
+                if coef > 0:
+                    self.angle = radians(90 * abs(coef))
+                else:
+                    self.angle = radians(90 + 90 * abs(coef))
+            hits = pygame.sprite.spritecollide(self, all_balls, False)
             if hits:
-                print(hits[0].rect.x)
-
+                for ball in hits:
+                    if pygame.sprite.collide_circle(self, ball):
+                        x_b, y_b = ball.rect.x, ball.rect.y
+                        if x_b <= x <= x_b + TILE_SIZE:
+                            self.angle = -self.angle
+                        else:
+                            self.angle = pi - self.angle
+                        ball.kill()
 
 
 class Border(pygame.sprite.Sprite):
@@ -155,15 +321,17 @@ class Border(pygame.sprite.Sprite):
 
 
 if __name__ == '__main__':
-    #screen = pygame.display.set_mode(WINDOW_SIZE)
-    pygame.display.set_caption('Арканоид на pygame ver 1.0.')
-    clock = pygame.time.Clock()
 
-    #start_screen()
+    pygame.display.set_caption('Арканоид на pygame ver 1.0.')
+    pygame.mouse.set_visible(False)
+    clock = pygame.time.Clock()
+    backgroud = pygame.transform.scale(load_image('background.jpg'), (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    start_screen()
 
     all_sprites = pygame.sprite.Group()
     all_balls = pygame.sprite.Group()
-    board = Board('map_1.txt')
+    board = Board(f"map_{MAPS_NUM}.txt")
     board.render()
 
     platform = Platform()
@@ -176,7 +344,10 @@ if __name__ == '__main__':
     Border(1, 0, WINDOW_WIDTH, 0)
     Border(0, WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
     Border(0, 0, 0, WINDOW_HEIGHT)
-    Border(WINDOW_WIDTH - 190, 0, WINDOW_WIDTH - 190, WINDOW_HEIGHT)
+    Border(WINDOW_WIDTH, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    pygame.mixer.music.load("sounds/bk_music.mp3")
+    pygame.mixer.music.play(-1)
 
     running = True
     moving = False
@@ -190,7 +361,24 @@ if __name__ == '__main__':
                 moving = True
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 moving = True
-        screen.fill('black')
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                moving = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                pygame.mixer.music.pause()
+                pause()
+                pygame.mixer.music.unpause()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                for ball in all_balls:
+                    ball.kill()
+        if check_win():
+            pygame.mixer.music.load('sounds/win_sound.mp3')
+            pygame.mixer.music.play(0)
+            show_win_message()
+        if check_lose():
+            pygame.mixer.music.load("sounds/lose_sound.mp3")
+            pygame.mixer.music.play(0)
+            show_lose_message()
+        screen.blit(backgroud, (0, 0))
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
